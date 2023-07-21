4c17afb9aa60e808d1404c6b87707554eb704c3e
Vivek Yadav <122459390+vivek5312@users.noreply.github.com>
2023-07-21 09:24:16 +0530

    Add component meal summary in app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Header from "./Layout/Header";
+import MealSummary from "./Meal/MealSummary";
 function App() {
   return (
     <div>
       <Header />
+      <MealSummary/>
     </div>
   );
 }
