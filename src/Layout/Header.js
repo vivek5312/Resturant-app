e24ff3186dcda3c42f4815708f89f5afc5ece952
Vivek Yadav <122459390+vivek5312@users.noreply.github.com>
2023-07-22 08:28:03 +0530

    Adding functionality cart open and close

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import mealImg from '../assest/meals.jpg'
-
+import HeaderButton from './HeaderButton'
 import classes from './Header.module.css'
-const Header = () => {
+const Header = (props) => {
   return (
     <>
     <header className={classes.header}>
-        <h1>ReactMrals</h1>
-        <button>Cart</button>
+        <h1>React Meals</h1>
+      <HeaderButton onClick={props.onCartShown}/>
     </header>
     <div className={classes['main-image']}> 
         <img src={mealImg} alt='A delicus food is waiting for you' ></img>
