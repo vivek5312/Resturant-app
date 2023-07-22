7ab18db00d995206be950ef97e3e4d93e8e08020
Vivek Yadav <122459390+vivek5312@users.noreply.github.com>
2023-07-22 08:26:44 +0530

    Adding functionality cart open and close

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import Header from "./Layout/Header";
 import Meal from "./Meal/Meal";
+import { useState } from "react";
+import Cart from "./Cart/Cart";
 function App() {
+  const[cartIsShown, setCartShown]=useState(false);
+
+  const setShownhandler=()=>{
+    setCartShown(true);
+  }
+  const hiddenCart=()=>{
+    setCartShown(false);
+  }
+
   return (
     <div>
-      <Header />
-     <Meal/>
+      {cartIsShown && <Cart onhidden={hiddenCart} />}
+      <Header  onCartShown={setShownhandler}/>
+     <Meal />
     </div>
   );
 }
